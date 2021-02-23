f6fc5d863f1e237792792a503ef9bb0daa280c1d
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-02-23 23:08:44 +0100

    Inserimento fontAwesome (mail)

--- mail/js/main.js
+++ mail/js/main.js
@@ -15,24 +15,26 @@ var submit = document.getElementById('submit');
 
 //controllo se userEmail è presente nell'array
 submit.addEventListener('click', 
-    function() {
+function() {
+    // inizializzazione variabile email utente
+    var userEmail = document.getElementById('email').value;
+    console.log(userEmail);
 
-        // inizializzazione variabile email utente
-        var userEmail = document.getElementById('email').value;
-        console.log(userEmail);
+    //inizializzazione variabile messaggio di uscita
+    var emailControll = document.getElementById('ms_email');
 
-        //inizializzazione variabile messaggio di uscita
-        var emailControll = document.getElementById('ms_email');
-
-        //confronto email con array
-        for (var i = 0; i < emailList.length; i++)
-            if (emailList[i] == userEmail) {
-                emailControll.className = "ms_success";
-                document.getElementById('ms_email').innerHTML = ("La tua email è presente nel nostro archivio. Puoi accedere");
-                break;
-            } else {
-                emailControll.className = "ms_error";
-                document.getElementById('ms_email').innerHTML = ("La tua email non è presente nel nostro archivio.")
-            }
+    //confronto email con array
+    for (var i = 0; i < emailList.length; i++)
+        if (emailList[i] == userEmail) {
+            document.getElementById('ms_success_check').style.display = 'block';
+            document.getElementById('ms_error_cross').style.display = 'none';
+            emailControll.className = "ms_success";
+            document.getElementById('ms_email').innerHTML = ("La tua email è presente nel nostro archivio. Puoi accedere");
+            break;
+        } else {
+            document.getElementById('ms_error_cross').style.display = 'block';
+            emailControll.className = "ms_error";
+            document.getElementById('ms_email').innerHTML = ("La tua email non è presente nel nostro archivio.")
+        }
     }
 )
